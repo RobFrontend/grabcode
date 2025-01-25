8e046e6e5cbe28c2f5a983f859f7e9fb3d077de3
RobFrontend <robert.grabowski97@gmail.com>
2025-01-25 02:23:37 +0100

    header logo

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pl">
       <head>
+        {/* Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NV9WKRJ9');
+            `,
+          }}
+        />
+        {/* JSON-LD - Dane strukturalne */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -41,7 +54,18 @@ export default function RootLayout({ children }) {
           }}
         />
       </head>
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        {/* Google Tag Manager <noscript> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NV9WKRJ9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
