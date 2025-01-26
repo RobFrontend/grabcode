1e990754d37658d0cabadab70279c6e19478a931
RobFrontend <robert.grabowski97@gmail.com>
2025-01-26 15:02:16 +0100

    mail

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,29 +1,29 @@
-"use server";
-import { Resend } from "resend";
+// "use server";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(userMail, userName, msg) {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: `GRABCODE STUDIO <onboarding@resend.dev>`,
-      to: ["robert.grabowski97@gmail.com"],
-      subject: userName,
-      html: `<h1>Email Klienta: ${userMail}</h1><br><//br><h2>Imię/Nazwa Klienta: ${userName}</h2><br><//br><p>${msg}</p>`,
-      // attachments: [
-      //   {
-      //     content,
-      //     filename,
-      //   },
-      // ],
-    });
+// export async function POST(userMail, userName, msg) {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: `GRABCODE STUDIO <onboarding@resend.dev>`,
+//       to: ["robert.grabowski97@gmail.com"],
+//       subject: userName,
+//       html: `<h1>Email Klienta: ${userMail}</h1><br><//br><h2>Imię/Nazwa Klienta: ${userName}</h2><br><//br><p>${msg}</p>`,
+//       // attachments: [
+//       //   {
+//       //     content,
+//       //     filename,
+//       //   },
+//       // ],
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json({ data });
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json({ data });
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
--- app/components/mailform.js
+++ app/components/mailform.js
@@ -1,63 +1,86 @@
 "use client";
 
-// import { sendEmail } from "@/lib/resend";
 import { POST } from "../api/send/route";
 import { useState } from "react";
 
 export default function ContactForm() {
-  const [text, setText] = useState("");
-  const [email, setEmail] = useState("");
-  const [userName, setUserName] = useState("");
-  // const [filename, setFilename] = useState("");
-  function handleText(e) {
-    setText(e.target.value);
-  }
-  function handleMail(e) {
-    setEmail(e.target.value);
-  }
-  function handleName(e) {
-    setUserName(e.target.value);
-  }
+  // const [text, setText] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [userName, setUserName] = useState("");
+  // // const [filename, setFilename] = useState("");
+  // function handleText(e) {
+  //   setText(e.target.value);
+  // }
+  // function handleMail(e) {
+  //   setEmail(e.target.value);
+  // }
+  // function handleName(e) {
+  //   setUserName(e.target.value);
+  // }
   // function handleFile(e) {
   //   setFilename(e.target.value);
   // }
-  function send() {
-    // sendEmail(text);
-    POST(email, userName, text);
+  // function send() {
+  //   POST(email, userName, text);
+  //   console.log(email, userName, text);
+  // }
 
-    // POST();
-    console.log(email, userName, text);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        access_key: process.env.NEXT_PUBLIC_MAIL_API_KEY,
+
+        name: e.target.name.value,
+        email: e.target.email.value,
+        message: e.target.message.value,
+      }),
+    });
+    const result = await response.json();
+    if (result.success) {
+      console.log(result);
+      console.log("wysłano");
+    } else {
+      console.log("nie działa");
+    }
   }
+
   return (
-    <form action={send} className="grid gap-4">
-      <label>
+    <form className="grid gap-4" onSubmit={handleSubmit}>
+      {/* <form action={send} className="grid gap-4"> */}
+      <label htmlFor="name">
         Imię/Nazwa:
         <input
           className="w-full h-24"
           type="text"
           name="name"
           required
-          value={userName}
-          onChange={(e) => handleName(e)}
+          // value={userName}
+          // onChange={(e) => handleName(e)}
         />
       </label>
-      <label>
+      <label htmlFor="email">
         E-mail:
         <input
           className="w-full h-24"
           type="email"
           name="email"
-          required
-          onChange={(e) => handleMail(e)}
+          // required
+          // onChange={(e) => handleMail(e)}
         />
       </label>
-      <label>
+      <label htmlFor="message">
         Wiadomość:
         <textarea
           className="w-full h-24"
           name="message"
           required
-          onChange={(e) => handleText(e)}
+          // onChange={(e) => handleText(e)}
         ></textarea>
       </label>
       {/* <label>
