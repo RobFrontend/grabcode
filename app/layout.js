94ab7206b07c9ecf972a3f3a428eb6ce7750ce2f
RobFrontend <robert.grabowski97@gmail.com>
2025-02-03 15:07:13 +0100

    icon

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,9 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/icon.png" />
+        <link rel="canonical" href="https://grabcode.pl" />
         {/* Google Tag Manager Script */}
         <script
           dangerouslySetInnerHTML={{
